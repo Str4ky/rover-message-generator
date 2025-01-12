3820eb2194b682fff30a6b4f7767e32f2b1636dd
Tim <38327800+Str4ky@users.noreply.github.com>
2025-01-12 19:57:57 +0100

    fix latency and font

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,17 +24,23 @@ function save() {
     const saveButton = document.getElementById("save");
 
     saveButton.innerHTML = '<i class="fa-solid fa-check"></i> Done';
-    setTimeout(() => {
-        saveButton.innerHTML = '<i class="fa-solid fa-floppy-disk"></i> Save to disk';
-    }, 1000);
 
-    htmlToImage.toBlob(container)
-        .then(function(blob) {
-            saveAs(blob, 'rover.png');
-        })
-        .catch(function(error) {
-            console.error('oops, something went wrong!', error);
+    html2canvas(container, { useCORS: true }).then((canvas) => {
+        canvas.toBlob((blob) => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'rover.png';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            setTimeout(() => {
+                saveButton.innerHTML = '<i class="fa-solid fa-floppy-disk"></i> Save to computer';
+            }, 1000);
         });
+    }).catch((error) => {
+        console.error('oops, something went wrong!', error);
+        saveButton.innerHTML = '<i class="fa-solid fa-floppy-disk"></i> Save to computer';
+    });
 }
 
 function copy() {
@@ -42,27 +48,28 @@ function copy() {
     const copyButton = document.getElementById("copy");
 
     copyButton.innerHTML = '<i class="fa-solid fa-check"></i> Done';
-    setTimeout(() => {
-        copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy to clipboard';
-    }, 1000);
 
-    htmlToImage.toBlob(container)
-        .then((blob) => {
-            setTimeout(() => {
+    html2canvas(container, { useCORS: true }).then((canvas) => {
+        canvas.toBlob((blob) => {
+            if (document.hasFocus()) {
                 navigator.clipboard.write([
-                    new ClipboardItem({
-                        'image/png': blob
-                    })
+                    new ClipboardItem({ 'image/png': blob })
                 ])
                 .then(() => {
-                    console.log("Image copied to clipboard successfully!");
+                    setTimeout(() => {
+                        copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy to clipboard';
+                    }, 1000);
                 })
                 .catch((error) => {
-                    console.error("Failed to copy image to clipboard:", error);
+                    console.error('oops, something went wrong!', error);
+                    copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy to clipboard';
                 });
-            }, 0);
-        })
-        .catch((error) => {
-            console.error('oops, something went wrong!', error);
+            } else {
+                copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy to clipboard';
+            }
         });
+    }).catch((error) => {
+        console.error('oops, something went wrong!', error);
+        copyButton.innerHTML = '<i class="fa-solid fa-clipboard"></i> Copy to clipboard';
+    });
 }
